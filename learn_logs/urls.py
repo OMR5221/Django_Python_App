3b2733c7522994545504aed42941883c6a3461fa
GinkoDante <ginkodante@gmail.com>
2018-08-11 23:52:42 -0400

    0811: Added homepage

--- learn_logs/urls.py
+++ learn_logs/urls.py
@@ -1,11 +1,19 @@
 """Defines URL patterns for learning_logs app"""
 
+# import path function to map url to views:
 from django.urls import path
 
+# import all of the views for the app
 from . import views
 
+# Helps Django differentiate this urls.py file from others:
 app_name = 'learn_logs'
+
+# pages that can be requested from the app:
 urlpatterns = [
 	# Home Page for learn_logs site:
+	# param 1: string to help Django route the current request properly when matched calls the 2nd aparam
+	# param 2: specify the view function to call
+	# param 3: a name we can reference this url by in the rest of the code
 	path('', views.index, name='index'),
 ]
--- learn_logs/views.py
+++ learn_logs/views.py
@@ -1,3 +1,10 @@
+# Renders the response based on the data provided by views:
 from django.shortcuts import render
 
-# Create your views here.
+# when url matches the pattern defined Django will look for a fucntion called index() here:
+def index(request):
+
+  """The home page for Learn Logs"""
+  # Param 1: the original request by user
+  # Param 2: the template to be called
+  return render(request, 'learn_logs/index.html')
